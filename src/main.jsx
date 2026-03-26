3201aa38fa4ee610528c07eaa55363e1ff937ea0
Colton Griffith <109363699+coltongriffith@users.noreply.github.com>
2026-03-25 18:49:14 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Buffer } from "buffer";
 import App from "./App";
 import "./styles.css";
+
+if (typeof window.global === "undefined") {
+  window.global = window;
+}
+
+if (typeof globalThis.Buffer === "undefined") {
+  globalThis.Buffer = Buffer;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
